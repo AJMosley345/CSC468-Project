841c6e013cebf9dde77b5707960e9fa064306795
AJ Mosley <ajmosley345@gmail.com>
2023-03-30 08:02:51 -0400

    Filled in Courses db

--- getClasses.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-response = requests.get("https://www.wcupa.edu/sciences-mathematics/computerScience/undergradCourses.aspx")
-
-soup = BeautifulSoup(response.content, 'html.parser')
-text_table=soup.select_one("table:nth-of-type(n)").get_text()
-table = soup.find('table', class_='responsiveTable').get_text()
-print(text_table)
