10c021b908d146d3462c95833a9184c1b3e14c91
Anthony Mosley <59812149+AJMosley345@users.noreply.github.com>
2023-04-12 20:57:54 -0400

    Create init.sql

---
+++ database/init.sql
@@ -0,0 +1,150 @@
+CREATE USER 'remote'@'webui' IDENTIFIED WITH mysql_native_password BY 'test1234';
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'remote'@'webui' WITH GRANT OPTION;
+
+CREATE TABLE students (
+    `student_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(250),
+    `pass` VARCHAR(250) DEFAULT NULL,
+    `fullName` VARCHAR(250)
+);
+
+CREATE TABLE `professors` (
+    `professor_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(250),
+    `pass` VARCHAR(250) DEFAULT NULL,
+    `fullName` VARCHAR(250)
+);
+CREATE TABLE `courses` (
+    `course_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `course_number` VARCHAR(250),
+    `course_name` VARCHAR(250)
+);
+
+CREATE TABLE `meetings` (
+    `meeting_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE  `student_courses` (
+    `student_id` INT NOT NULL,
+    `course_id` INT NOT NULL,
+    PRIMARY KEY (`student_id`, `course_id`),
+    FOREIGN KEY (`student_id`) REFERENCES `students`(`student_id`),
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`)
+);
+
+CREATE TABLE professor_courses (
+    `professor_id` INT NOT NULL,
+    `course_id` INT NOT NULL,
+    PRIMARY KEY (`professor_id`, `course_id`),
+    FOREIGN KEY (`professor_id`) REFERENCES `professors`(`professor_id`),
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`)
+);
+
+
+INSERT INTO 
+    `students` (`username`, `pass`, `fullName`)
+VALUES 
+    ("AM01", MD5("J98xWOEEo"), "Anthony Mosley"),
+    ("AZ02", MD5("oAm1yI8SQ"), "Aroum Zombra"),
+    ("MB03", MD5("4aPJACpup"), "Michael Burns"),
+    ("JH04", MD5("bVYFdjWB3"), "Joey Haywood"),
+    ("HK05", MD5("4W6l9q9w3"), "Hayden Kanak");
+
+INSERT INTO
+    `professors` (`username`, `pass`, `fullName`)
+VALUES
+    ("TProfessor", MD5("Z14VdAlyK"), "Test Professor"),
+    ("TProfessor2", MD5("9fbzmBWeL"), "Test Professor2"),
+    ("TProfessor3", MD5("XS98PtClR"), "Test Professor3"),
+    ("TProfessor4", MD5("XqetLj4bO"), "Test Professor4");
+
+INSERT INTO
+    `courses` (`course_number`, `course_name`)
+VALUES
+    ('CSC 112','Programming & Data Science'), --  1
+    ('CSC 115','Introduction to Computer Programming'), -- 2
+    ('CSC 141','Computer Science I'), -- 3
+    ('CSC 142','Computer Science II'), -- 4
+    ('CSC 220','Foundations of Computer Science'), -- 5
+    ('CSC 231','Computer Systems'), -- 6
+    ('CSC 240','Computer Science III'),-- 7
+    ('CSC 241','Data Structures and Algorithms'),-- 8
+    ('CSC 242','Computer Organization'),-- 9
+    ('CSC 301','Computer Security & Ethics'),-- 10
+    ('CSC 302','Computer Security'),-- 11
+    ('CSC 317','Introduction to Digital Image Processing'),-- 12
+    ('CSC 321','Database Management Systems'),-- 13
+    ('CSC 331','Operating Systems'),-- 14
+    ('CSC 335','Data Communications and Networking I'),-- 15
+    ('CSC 336','Data Communications and Networking II'),-- 16
+    ('CSC 345','Programming Language Concepts and Paradigms'),-- 17
+    ('CSC 400','Internship'), -- 18
+    ('CSC 402','Software Engineering'),-- 19
+    ('CSC 404','Software Engineering & Testing'),-- 20
+    ('CSC 416','Design and Construction of Compilers'),-- 21
+    ('CSC 417','User Interfaces'),-- 22
+    ('CSC 466','Distributed and Parallel Programming'),-- 23
+    ('CSC 467','Big Data Engineering'),-- 24
+    ('CSC 468','Introduction to Cloud Computing'),-- 25
+    ('CSC 471','Modern Malware Analysis'),-- 26
+    ('CSC 472','Software Security'),-- 27
+    ('CSC 476','Game Development'),-- 28
+    ('CSC 481','Artificial Intelligence'),-- 29
+    ('CSC 490','Independent Project'),-- 30
+    ('CSC 495','Topics in Computer Science'),-- 31
+    ('CSC 496','Topics in Complex Systems'),-- 32
+    ('CSC 497','Topics in Computer Security'),-- 33
+    ('CSC 499','Independent Study in Computer Science');-- 34
+
+INSERT INTO
+    `student_courses` (`student_id`, `course_id`)
+VALUES
+    -- INSERT into student with student_id 1
+    (1, 2),
+    (1, 3),
+    (1, 23),
+    (1, 15),
+    -- INSERT into student with student_id 2
+    (2, 2),
+    (2, 23),
+    (2, 15),
+    (2, 19),
+    -- INSERT into student with student_id 3
+    (3, 12),
+    (3, 31),
+    (3, 16),
+    (3, 5),
+    -- INSERT into student with student_id 4
+    (4, 12),
+    (4, 23),
+    (4, 26),
+    (4, 11),
+    -- INSERT into student with student_id 5
+    (5, 18),
+    (5, 32),
+    (5, 25),
+    (5, 28);
+
+INSERT INTO
+    `professor_courses` (`professor_id`, `course_id`)
+VALUES
+    -- INSERT into professor with professor_id 1
+    (1, 2),
+    (1, 3),
+    (1, 23),
+    (1, 15),
+    -- INSERT into professor with professor_id 2
+    (2, 2),
+    (2, 23),
+    (2, 15),
+    (2, 19),
+    -- INSERT into professor with professor_id 3
+    (3, 12),
+    (3, 31),
+    (3, 16),
+    (3, 5),
+    -- INSERT into professor with professor_id 4
+    (4, 12),
+    (4, 23),
+    (4, 26),
+    (4, 11);
