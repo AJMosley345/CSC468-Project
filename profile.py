753153dd7ef6a3cce777e80e8af367896cbdd2e4
AJMosley <ajmosley345@gmail.com>
2023-04-20 07:00:09 -0400

    Add jenkins files

--- profile.py
+++ profile.py
@@ -1,21 +1,52 @@
-"""An example of constructing a profile with a single Xen VM.
-
-Instructions:
-Wait for the profile instance to start, and then log in to the VM via the
-ssh port specified below.  (Note that in this case, you will need to access
-the VM through a high port on the physical host, since we have not requested
-a public IP address for the VM itself.)
-"""
-
 # Import the Portal object.
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
+import geni.rspec.igext as IG
 
-# Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
- 
-# Add a XenVM (named "node") to the request
-node = request.XenVM("node")
+pc = portal.Context()
+request = pc.makeRequestRSpec()
 
-# Write the request in RSpec format
-portal.context.printRequestRSpec()
+tourDescription = \
+"""
+This profile is a base for the project to test on
+"""
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+num_nodes = 3
+for i in range(num_nodes):
+  if i == 0:
+    node = request.XenVM("head")
+    bs_landing = node.Blockstore("bs_image", "/image")
+    bs_landing.size = "500GB"
+  else:
+    node = request.XenVM("worker-" + str(i))
+  node.cores = 4
+  node.ram = 8192
+  node.routable_control_ip = "true" 
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
+  # setup Docker
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+  # setup Kubernetes
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
+  
+  if i == 0:
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
+  else:
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
+    
+pc.printRequestRSpec(request)
