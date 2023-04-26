3edd48bf43c57c6f8ca1e60d86503957f7990a48
AJ Mosley <ajmosley345@gmail.com>
2023-04-26 09:30:30 -0400

    Modify Dockerfile

--- webui/next.config.cjs
+++ webui/next.config.cjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'standalone',
+  outputStandalone: true,
   eslint: {
     ignoreDuringBuilds: true,
   }
--- webui/src/pages/_app.tsx
+++ webui/src/pages/_app.tsx
@@ -1,14 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../../components/Layout'
-import { SessionProvider } from 'next-auth/react';
-
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '../../components/Layout';
 export default function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
   return(
-    <SessionProvider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
