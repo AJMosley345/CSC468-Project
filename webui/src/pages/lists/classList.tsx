27aff1f5f684b2dd7a7d05f3e34ff00dfc20a3d5
AJ Mosley <ajmosley345@gmail.com>
2023-04-27 10:25:25 -0400

    Modify classlist.tsx

--- webui/src/pages/lists/classList.tsx
+++ webui/src/pages/lists/classList.tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps } from "next";
-import { Course } from "../interfaces";
+import { Course } from "../../../interfaces";
 import React from "react";
 import { Stack, Typography } from "@mui/material";
-import { prisma } from "../../lib/db";
-import CourseList from "../components/CourseList";
+import { prisma } from "../../../lib/db";
+import CourseList from "../../../components/CourseList";
 
 export const getStaticProps: GetStaticProps = async () => {
     const courseList = await prisma.courses.findMany();
