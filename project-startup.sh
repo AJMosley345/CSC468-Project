8de9d7e68434dcea2eeb1f60c9972733a7ca709d
AJ Mosley <ajmosley345@gmail.com>
2023-05-03 14:03:42 -0400

    Modiy project startup

--- project-startup.sh
+++ project-startup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -x
 
-# docker-compose -f docker-compose.images.yml build
-# docker-compose -f docker-compose.images.yml push
 namespaceStatus=$(kubectl get namespaces project -o json | jq .status.phase -r)
 
 if [ $namespaceStatus == "Active" ]
@@ -16,6 +14,13 @@ kubectl create namespace project
 kubectl create configmap startup --from-file=./webui/startup.sh -n project
 
 echo "Creating pods and services"
-kubectl create -f project-deployment.yaml -n project
+kubectl create -f db-deployment.yaml -n project
+
+docker-compose -f docker-compose.images.yml build
+docker-compose -f docker-compose.images.yml push
+
+sleep 20
+
+kubectl create -f webui-deployment.yaml -n project
 
 kubectl get pods -n project
