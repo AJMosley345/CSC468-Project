8cd8cbb9ff9f558160e606ba75aebe3bf3c91842
Anthony Mosley <59812149+AJMosley345@users.noreply.github.com>
2023-05-02 21:12:34 -0400

    Update project-startup.sh

--- project-startup.sh
+++ project-startup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -x
 
-# docker-compose -f docker-compose.images.yml build
-# docker-compose -f docker-compose.images.yml push
+docker-compose -f docker-compose.images.yml build
+docker-compose -f docker-compose.images.yml push
 
-# namespaceStatus=$(kubectl get namespaces project -o json | jq .status.phase -r)
+namespaceStatus=$(kubectl get namespaces project -o json | jq .status.phase -r)
 
-# if [ $namespaceStatus == "Active" ]
-# then
-#     echo "Namespace project exists, need to clean up"
-#     kubectl delete namespaces project
-# fi
+if [ $namespaceStatus == "Active" ]
+then
+     echo "Namespace project exists, need to clean up"
+     kubectl delete namespaces project
+ fi
 
 echo "Creating namespace project"
 kubectl create namespace project
