8b9b60434988f794b72be96a3a6015cd924602d4
AJ Mosley <ajmosley345@gmail.com>
2023-05-03 19:42:45 -0400

    Modify startup

--- webui/startup.sh
+++ webui/startup.sh
@@ -2,7 +2,7 @@
 set -x
 cd /app
 sleep 10
-export DATABASE_URL="mysql://root:test1234@localhost:3306/project?schema=public"
+DATABASE_URL="mysql://root:test1234@localhost:3306/project?schema=public"
 npx prisma generate
 npx prisma migrate deploy
 npx prisma db seed
