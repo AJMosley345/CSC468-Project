3e98d9334907efc1ab45b70a02364d89c5bda2d7
AJ Mosley <ajmosley345@gmail.com>
2023-05-04 08:51:26 -0400

    Add nginx-deployment and modify project-startup.sh

--- project-startup.sh
+++ project-startup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -x
 
-namespaceStatus=$(kubectl get namespaces project -o json | jq .status.phase -r)
+# namespaceStatus=$(kubectl get namespaces project -o json | jq .status.phase -r)
 
-if [ $namespaceStatus == "Active" ]
-then
-     echo "Namespace project exists, need to clean up"
-     kubectl delete namespaces project
- fi
+# if [ $namespaceStatus == "Active" ]
+# then
+#      echo "Namespace project exists, need to clean up"
+#      kubectl delete namespaces project
+# fi
 
-echo "Creating namespace project"
-kubectl create namespace project
+# echo "Creating namespace project"
+# kubectl create namespace project
 # kubectl create configmap startup --from-file=./webui/startup.sh -n project
 
 echo "Creating pods and services"
-kubectl create -f db-deployment.yaml -n project
+kubectl create -f db-deployment.yaml 
 sleep 30
-kubectl create -f webui-deployment.yaml -n project
+kubectl create -f webui-deployment.yaml 
+sleep 30
+kubectl create -f nginx-deployment.yaml
 
 kubectl get pods -n project
