b81094b39f8be11c4f69b5d04d061ab9c47889d8
AJ Mosley <ajmosley345@gmail.com>
2023-05-04 21:26:50 -0400

    Finally done

--- project-startup.sh
+++ project-startup.sh
@@ -17,9 +17,6 @@ kubectl create -f db-deployment.yaml -n project
 sleep 30
 kubectl create -f webui-deployment.yaml -n project
 
-kubectl expose deploy/webui --port=3000 --type=NodePort
-export NODE_PORT=$(kubectl get --namespace project -o jsonpath="{.spec.ports[0].nodePort}" services webui)
-export NODE_IP=$(kubectl get nodes --namespace project -o jsonpath="{.items[0].status.addresses[0].address}")
-echo http://$NODE_IP:$NODE_PORT/login
+kubectl expose deploy/webui --port=3000 --type=NodePort -n project
 
 kubectl get pods -n project
